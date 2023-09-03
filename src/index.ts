a2e966deced8f6e124ab1125e1e8d3656d4f7e5f
Deen-q <124711768+Deen-q@users.noreply.github.com>
2023-09-03 17:17:56 +0100

    added some comments to explain bundlers in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// Snowpack is a bundler --> needed so browser can access node_modules folder (bundles the folder so browser can understand it)
+// All files were deleted, if any, then 'npx create-snowpack-app . --template @snowpack/app-template-blank-typescript' was used
+// '.' means it occurs in this folder. '--force' to enable that, if it gives an error.
+
 /**
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
